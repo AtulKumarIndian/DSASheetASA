d025e810f617d94c273f1902b549e2501abfe121
AtulKumarIndian <atulkrindian@gmail.com>
2022-01-27 17:10:04 +0530

    rowwithMax1

---
+++ RowWithMaxOne.cpp
@@ -0,0 +1,15 @@
+/*
+Given a boolean 2D array of n x m dimensions where each row is sorted.
+Find the 0-based index of the first row that has the maximum number of 1's.*/
+ #include<iostream>
+ #include<vector>
+using namespace std;
+  int rowWithMax1s(int a[][], int n, int m){
+       for(int i=0, int j =0; i<n, j<m; i++)
+    {
+        if(a[i][j]== 1) return i;
+        if(i = 3) {i%=3; j++}
+    } return -1;
+  } 
+   
+
